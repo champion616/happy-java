2eadbd9551cb0ef4c8195c2c5b0fcf7617931707
shl811 <suhyeonlee811@gmail.com>
2023-06-19 21:34:04 +0900

    first commit

---
+++ src/MyProfile.java
@@ -0,0 +1,5 @@
+public class MyProfile {
+    public static void main(String[] args){
+        System.out.printf("이름: %s, 성별: %s, 이메일주소: %s", "이수현", "여성", "justbethechampion@gmail.com");
+    }
+}
---
+++ src/Rectangle2.java
@@ -0,0 +1,9 @@
+public class Rectangle2 {
+    public static void main(String[] args){
+        int i = 0;
+        while(i < 1000){
+            System.out.println("**********");
+            i++;
+        }
+    }
+}
