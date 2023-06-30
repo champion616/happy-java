33e6dbaa4bd9ee3aa130a6023117309d4875e6dd
champion616 <justbethechampion@gmail.com>
2023-06-30 21:23:56 +0900

    6/30

---
+++ src/섹션7/JavaIO/composite/Node.java
@@ -0,0 +1,20 @@
+package 섹션7.JavaIO.composite;
+
+public abstract class Node {
+    private String name;
+
+    public Node(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract long getSize();
+    public abstract boolean isFolder();
+}
